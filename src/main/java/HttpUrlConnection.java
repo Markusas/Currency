57cf536693ec85216cf97589c5b9ebead90ef808
Marek Butrimas <Marek.Butrimas@sai360.com>
2024-01-26 16:26:43 +0200

    Check now my commit

--- src/main/java/HttpUrlConnection.java
+++ src/main/java/HttpUrlConnection.java
@@ -8,6 +8,7 @@ public class HttpUrlConnection {
     static String API_KEY = "f3a1b7e47c449089b21c0fd5cc904d59";
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        //I will move it to another class
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(URI.create(String.format("http://data.fixer.io/api/latest?access_key=%s", API_KEY)))
                 .header("accept", "application/json")
