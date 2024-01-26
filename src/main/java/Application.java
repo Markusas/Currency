851ed43d62ff04435fa6b205707e903436e08992
Marek Butrimas <Marek.Butrimas@sai360.com>
2024-01-26 17:48:45 +0200

    Remove var, separate logic from application

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        HttpUrlConnection connection = new HttpUrlConnection();
+        connection.getAllcurrency();
+    }
+}
--- src/main/java/HttpUrlConnection.java
+++ src/main/java/HttpUrlConnection.java
@@ -1,21 +1,18 @@
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class HttpUrlConnection {
-    static String API_KEY = "f3a1b7e47c449089b21c0fd5cc904d59";
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //I will move it to another class
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(URI.create(String.format("http://data.fixer.io/api/latest?access_key=%s", API_KEY)))
+    public void getAllcurrency() throws Exception {
+        String API_KEY = "f3a1b7e47c449089b21c0fd5cc904d59";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(String.format("http://data.fixer.io/api/latest?access_key=%s", API_KEY)))
                 .header("accept", "application/json")
                 .build();
-
-        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
     }
+
+
 }
