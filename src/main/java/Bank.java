1cee84a3b82946d71f9e72d752be9cd99648a37e
Marek Butrimas <Marek.Butrimas@sai360.com>
2024-01-29 17:05:39 +0200

    Add fields for app to deserialeze

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,4 +1,11 @@
+import java.util.Map;
+
 public class Bank {
+    private boolean success;
+    private String timestamp;
+    private String base;
+    private String date;
+    private Map<String, Double> rates;
 
     public static void main(String[] args) throws Exception {
         HttpUrlConnection connection = new HttpUrlConnection();
