d109354558bd404618fb9075468c51af8702a896
Marek Butrimas <Marek.Butrimas@sai360.com>
2024-01-29 17:14:21 +0200

    Remove sout from logic

--- src/main/java/HttpUrlConnection.java
+++ src/main/java/HttpUrlConnection.java
@@ -4,14 +4,14 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class HttpUrlConnection {
-    public void getAllcurrency() throws Exception {
+    public String getAllcurrency() throws Exception {
         String API_KEY = "f3a1b7e47c449089b21c0fd5cc904d59";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(URI.create(String.format("http://data.fixer.io/api/latest?access_key=%s", API_KEY)))
                 .header("accept", "application/json")
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        return response.body();
     }
 
 
