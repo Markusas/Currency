a3b1543513d4c5d14ddccb405c3f7399889fcbba
Marek Butrimas <Marek.Butrimas@sai360.com>
2024-01-29 17:06:28 +0200

    Add setters and getters

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -7,6 +7,46 @@ public class Bank {
     private String date;
     private Map<String, Double> rates;
 
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Map<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, Double> rates) {
+        this.rates = rates;
+    }
+
     public static void main(String[] args) throws Exception {
         HttpUrlConnection connection = new HttpUrlConnection();
         connection.getAllcurrency();
